e178924fd47a9d9fc8c8c0d1aefdf973a1d26b3e
lucad <luca.delmarche@gmail.com>
2025-10-13 11:27:36 +0200

    Add Price property to Item and update database migration

---
+++ Infrastructure/Migrations/20251013092012_ItemsPriceAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemsPriceAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "price",
+                table: "items",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "items");
+        }
+    }
+}
